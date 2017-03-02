41060aff65469a6abcf30f1f08784ca3d1160e2e
Daniel Madrid <daniel.madrid.reina@gmail.com>
2017-03-02 17:28:15 -0500

    Allow durationize unit option
    Takes a column name to extract unit name from ('hours', 'minutes', etc)
    
    Adds a method to retrieve the value according to specified units.

--- lib/durationizer.rb
+++ lib/durationizer.rb
@@ -8,18 +8,22 @@ module Durationizer
   end
 
   module ClassMethods
-    def durationize(*columns)
-      columns.each do |column|
-        reader_name = column.to_s.sub(/_in_seconds$/, '')
-        writer_name = "#{reader_name}="
+    def durationize(column, options = {})
+      reader_name = column.to_s.sub(/_in_seconds$/, '')
+      writer_name = "#{reader_name}="
+      unit_column = options.fetch(:unit, "#{reader_name}_unit")
 
-        define_method reader_name do
-          public_send(column).seconds
-        end
+      define_method reader_name do
+        public_send(column).seconds
+      end
 
-        define_method writer_name do |duration|
-          public_send("#{column}=", duration)
-        end
+      define_method writer_name do |duration|
+        public_send("#{column}=", duration)
+      end
+
+      define_method "#{reader_name}_in_units" do
+        unit_value = public_send(unit_column)
+        public_send(reader_name) / 1.send(unit_value)
       end
     end
   end
--- test/durationizer_test.rb
+++ test/durationizer_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 describe Durationizer do
   def setup
     @dummy = DummyModel.new
+    @dummy.delay_time_unit = 'hours'
     @dummy.delay_time_in_seconds = 3_600
   end
 
@@ -23,6 +24,12 @@ describe Durationizer do
 
       assert_equal 1_800, @dummy.delay_time_in_seconds
       assert_equal 30.minutes, @dummy.delay_time
+    end
+  end
+
+  context '#delay_time_in_units' do
+    it 'should return number of hours' do
+      assert_equal 1, @dummy.delay_time_in_units
     end
   end
 end
