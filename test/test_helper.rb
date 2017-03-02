8a5ac10aa200126552b640140f2ffe64ac1f28d8
Lucas Lessa <lucas@skyverge.com>
2017-03-02 14:17:46 -0500

    DummyModel for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,12 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'durationizer'
 require 'minitest/autorun'
 require 'minitest/pride'
+
+class DummyModel
+  attr_accessor :hour_in_seconds, :two_years_in_seconds
+
+  def initialize
+    @hour_in_seconds = 3600
+    @two_years_in_seconds = 63_072_000
+  end
+end
